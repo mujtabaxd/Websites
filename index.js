1dd86995efedc3e19f1812e418fe211ee01d2904
mujtabaxd <mujtabaviva@gmail.com>
2022-07-24 22:31:28 +0500

    fixed code

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
+
 function getRecaptchaToken() {
   grecaptcha.ready(function () {
-    grecaptcha.execute('6Le8iRAhAAAAAPMXtuCuqxI07KZ48Vsa2GSIRYwJ', { action: 'verifyToken' }).then(function (token) {
-      console.log(token)
-      fetch('/verify.php?action=verifyToken&response=' + token)
-        .then(response => response.text())
-    });
+    grecaptcha.execute('6Le8iRAhAAAAAPMXtuCuqxI07KZ48Vsa2GSIRYwJ', { action: 'verifyToken' })
+      .then((token) => {
+        console.log('Token retrieved. Sending request to backend...')
+        verifyRecaptchaToken(token)
+      });
   });
 }
 
-function testurl() {
+function verifyRecaptchaToken(token) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  fetch('verify.php?token=' + token, requestOptions)
+    .then((response) => {
+      return response.text()
+    })
+    .then(data => {
+      // console.log(data)
+      var respDiv = document.getElementById('response')
+      respDiv.innerHTML = data
+    })
+    .catch(error => console.log('site-verify-error', error));
+}
+
+function test() {
   var requestOptions = {
-    method: 'GET',
-    data: 'SampleData',
-    response: "Token",
-    action: 'teset'
+    method: 'POST',
   };
 
-  fetch("verify.php", requestOptions)
+  fetch("verify.php?token=test-token", requestOptions)
     .then((response) => {
       return response.text()
     })
@@ -24,6 +42,9 @@ function testurl() {
       console.log(text);
       const respDiv = document.getElementById('response')
       respDiv.innerHTML = text
+      // if (text.match('#response')) {
+      //   respDiv.innerHTML = text
+      // }
     })
     .catch((error) => {
       console.error(error);
--- verify.php
+++ verify.php
@@ -1,14 +1,56 @@
 <?php
-$elem = '<div class="card response-body p-2" id="response">success</div>';
-if (isset($elem)) {
-    print($elem);
-    return;
-}
-$data = array(
-    'GET'=> $_GET,
-    'POST'=> $_POST,
-    'REQUEST'=> $_REQUEST,
-    'SERVER'=> $_SERVER,
-);
 
-print("<pre>".print_r($data,true)."</pre>");
+function pprint($data)
+{
+    // Pretty prints your text or object
+    return "<pre>";
+    print_r($data, true);
+    "</pre>";
+}
+
+function requestDump()
+{
+    $data = array(
+        'GET' => $_GET,
+        'POST' => $_POST,
+        'REQUEST' => $_REQUEST,
+        'SERVER' => $_SERVER,
+    );
+    return "<pre>" . print_r($data) . "<pre>";
+}
+
+function verifyCaptcha()
+{
+    $token = $_REQUEST['token'];
+    $SECRET = '6Le8iRAhAAAAAOkG6b8Pt8pPnaMV4VFrTH3i2SZK';
+
+    $url = "https://www.google.com/recaptcha/api/siteverify?secret=$SECRET&response=$token";
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $headers = array(
+        "Content-Type: application/json",
+    );
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    $resp = curl_exec($curl);
+    // echo($resp);
+    // print('<div class="card response-body p-2" id="response">'.$resp.'</div>');
+    print($resp);
+    curl_close($curl);
+}
+
+
+// if (isset($_POST)){
+//     $response = $_POST['token'];
+// }
+
+verifyCaptcha();
+
+// $elem = '<div class="card response-body p-2" id="response">success</div>';
+// print($elem);
+
+// requestDump();
+
